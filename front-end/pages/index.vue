49fab3049eea9f8dbf32f99ffae5b6e938d96e21
Maysa <maysa.bonfante@webcore.com.br>
2025-03-11 19:59:16 -0300

    add front-end

---
+++ front-end/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <section class="prompt">
+			<h3>new project</h3>
+			<h1>Sign in</h1>
+		
+			<section class="messages">
+				
+			</section>
+			<a class="button facebook" href="http://localhost:4000/login">Sign in with Google</a>
+		</section>
+    </div>
+</template>
+
+<script>
+    
+</script>
