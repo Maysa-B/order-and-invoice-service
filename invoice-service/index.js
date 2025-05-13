c608cc6a14bffe2cd2a00da43323651dfe2ec042
Maysa <maysa.bonfante@webcore.com.br>
2025-05-12 21:21:23 -0300

    fix: invoice service kafka consumer

--- invoice-service/index.js
+++ invoice-service/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { consumeError, consumeSuccess } = require('./helpers/kafka')
+const { consumePayment } = require('./helpers/kafka')
 require('dotenv').config()
 
 const app = express()
@@ -8,7 +8,6 @@ const app = express()
 app.use(express.json())
 app.use(cors({ origin: true, credentials: true, maxAge: 7 * 24 * 3600 }))
 
-consumeError()
-consumeSuccess()
+consumePayment()
 
 app.listen(4003, () => console.log("Invoice Service running on port 4003"));
